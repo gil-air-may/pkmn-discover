04e71666066476b7eccc16691729aca3b53fc367
Filipe Gutierry <filipe@kenzie.com.br>
2020-08-07 18:13:42 -0300

    chore: basic logic

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -5,7 +5,7 @@ import { CardStyled } from "./styles";
 const Card = ({ pokemon }) => {
   const { data, loading, error } = usePalette(pokemon.sprites.front_default);
 
-  console.log(data);
+  //console.log(data);
   return (
     <CardStyled bgColor={data.vibrant}>
       <img style={{ width: "300px" }} src={pokemon.sprites.front_default} />
