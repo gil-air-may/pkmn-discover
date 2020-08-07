6c9a20ebcafaefbe658ee5a646630aa4201d5507
Filipe Gutierry <filipe.gutierry@gmail.com>
2020-08-07 18:48:09 -0300

    Color from pkmn (#1)
    * feat: extract color palette
    
    * chore: added styled component
    
    * chore: basic logic
    
    * add: pokemon font :)
    
    * comparative logic
    
    * monolitic game
    
    Co-authored-by: gilairmay <gilairmay@gmail.com>
    Co-authored-by: Filipe Gutierry <filipe@kenzie.com.br>

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { useEffect, useState } from "react";
 
-import "./App.css";
-
 import { selectRandomGen1 } from "./api";
+import Card from "./components/Card";
+
+import "./App.css";
 
 const App = () => {
   const [pokemon, setPokemon] = useState(undefined);
+  const [input, setInput] = useState("");
+  const [correctScore, setCorrectScore] = useState(0);
+  const [wrongScore, setWrongScore] = useState(0);
+  const [timer, setTimer] = useState(30);
 
   const fetchPokemon = async () => {
     const response = await selectRandomGen1();
@@ -14,16 +19,65 @@ const App = () => {
     setPokemon(pokemon);
   };
 
+  const tick = () => {
+    const counter = setTimeout(() => setTimer(timer - 1), 1000);
+    if (timer === 0) {
+      clearTimeout(counter);
+    }
+  };
+
+  const resetGame = () => {
+    setTimer(30);
+    setWrongScore(0);
+    setCorrectScore(0);
+    setInput("");
+    tick();
+    fetchPokemon();
+  };
+
+  useEffect(() => {
+    tick();
+  }, [timer]);
+
   useEffect(() => {
     fetchPokemon();
   }, []);
 
+  const handleInput = (e) => {
+    setInput(e.target.value);
+  };
+
+  const handleEnter = (e) => {
+    if (e.keyCode === 13) {
+      fetchPokemon();
+      setInput("");
+      if (input.toLowerCase() === pokemon.name) {
+        console.log("ACERTOUUU");
+        setCorrectScore(correctScore + 1);
+      } else {
+        setWrongScore(wrongScore + 1);
+        console.log("ERRROU =c");
+      }
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        {pokemon && (
-          <img style={{ width: "30%" }} src={pokemon.sprites.front_default} />
+        <h1>{timer}</h1>
+        <p>Acertos {correctScore}</p>
+        <p>Erros {wrongScore}</p>
+        {timer === 0 ? (
+          <button onClick={resetGame}>Try Again</button>
+        ) : (
+          <input
+            type="text"
+            value={input}
+            onChange={handleInput}
+            onKeyUp={handleEnter}
+          />
         )}
+        {pokemon && <Card pokemon={pokemon} />}
       </header>
     </div>
   );
---
+++ src/components/Card/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { usePalette } from "react-palette";
+import { CardStyled } from "./styles";
+
+const Card = ({ pokemon }) => {
+  const { data, loading, error } = usePalette(pokemon.sprites.front_default);
+
+  //console.log(data);
+  return (
+    <CardStyled bgColor={data.vibrant}>
+     <p>{pokemon.name}</p>
+      <img style={{ width: "300px" }} src={pokemon.sprites.front_default} />
+    </CardStyled>
+  );
+};
+
+export default Card;
